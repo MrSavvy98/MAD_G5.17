9f6b0aaa728ae41949e7f0539b4575a72e2d3773
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-08-16 12:12:26 +0530

    Home Page edited

--- app/src/main/java/com/example/workfromhome/home.java
+++ app/src/main/java/com/example/workfromhome/home.java
@@ -2,7 +2,10 @@ package com.example.workfromhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Button btn_attendance = findViewById(R.id.btn_attendance);
+
+        btn_attendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(home.this, Attendance.class );
+                startActivity(intent);
+            }
+        });
     }
 }
